1fc8a4be7ca695f94adc55328f5d0aff55b4ed57
Moshe36 <92290379+Moshe36@users.noreply.github.com>
2023-06-11 19:32:34 +0300

    Update my_program.cpp

--- my_program.cpp
+++ my_program.cpp
@@ -4,62 +4,41 @@
 #include <cmath>
 #include <ctime>
 #include <string>
+#include "matrix_double.h"
 
-typedef std::vector<std::vector<double>> Matrix;
+//typedef std::vector<std::vector<double>> Matrix;
 
 const double EPSILON = std::numeric_limits<double>::epsilon();
 const int MAX_FACT = 170;
 const int N = 10;
 const double ERROR_MARGIN = 1e-5;
 
-Matrix matrixFromFile(const std::string& filename)
-{
-    Matrix matrix;
-    std::ifstream file(filename);
-    if (file)
-    {
-        std::string line;
-        while (std::getline(file, line, '\n'))
-        {
-            std::vector<double> row;
-            size_t start = 0;
-            size_t end = line.find(',', start);
-            while (end != std::string::npos)
-            {
-                std::string numStr = line.substr(start, end - start);
-                double num = std::strtod(numStr.c_str(), nullptr);
-                row.push_back(num);
-                start = end + 1;
-                end = line.find(',', start);
-            }
-            std::string lastNumStr = line.substr(start);
-            double lastNum = std::strtod(lastNumStr.c_str(), nullptr);
-            row.push_back(lastNum);
-            matrix.push_back(row);
-        }
+//Create identity matrix
+Matrix createIdentityMatrix(int size) {
+    Matrix identity(size, size, 0.0);
+
+    for (int i = 0; i < size; i++) {
+        identity.p[i * size + i] = 1.0;
     }
-    return matrix;
+
+    return identity;
+}
+double calculateMatrixSum(const Matrix& matrix) {
+    double sum = 0.0;
+    for (int i = 0; i < matrix.rows * matrix.cols; i++) {
+        sum += matrix.p[i];
+    }
+    return sum;
 }
 
-bool validateSquare(const Matrix& matrix)
-{
-    size_t numRows = matrix.size();
-    size_t numCols = matrix[0].size();
-    return (numRows == numCols);
-}
-
-void resultToFile(const Matrix& resultMatrix, const std::string& filename)
-{
+void resultToFile(const Matrix& resultMatrix, const std::string& filename) {
     std::ofstream file(filename);
-    if (file)
-    {
-        for (const auto& row : resultMatrix)
-        {
-            for (size_t i = 0; i < row.size() - 1; i++)
-            {
-                file << row[i] << ",";
+    if (file) {
+        for (int i = 0; i < resultMatrix.rows; i++) {
+            for (int j = 0; j < resultMatrix.cols - 1; j++) {
+                file << resultMatrix.p[i * resultMatrix.cols + j] << ",";
             }
-            file << row.back() << "\n";
+            file << resultMatrix.p[i * resultMatrix.cols + resultMatrix.cols - 1] << "\n";
         }
     }
 }
@@ -83,55 +62,24 @@ double lagrange(const std::vector<double>& x, const std::vector<double>& y, doub
     return yi;
 }
 
-Matrix calculateExponential(const Matrix& matrix)
+Matrix calculateExponential(Matrix matrix)
 {
     size_t iterNum = 0;
-    Matrix result(matrix.size(), std::vector<double>(matrix[0].size(), 0.0));
+    Matrix result = createIdentityMatrix(matrix.cols);
     std::vector<double> x, y;
 
-    Matrix matrixPower(matrix.size(), std::vector<double>(matrix[0].size(), 0.0));
-    for (size_t i = 0; i < matrix.size(); i++)
-    {
-        matrixPower[i][i] = 1.0;  // Initialize matrixPower with the identity matrix
-    }
+    Matrix matrixPower = createIdentityMatrix(matrix.cols);
     double fact = 1.0;
 
     while (true)
     {
         if (iterNum > 0)
         {
-            Matrix tempPower(matrix.size(), std::vector<double>(matrix[0].size(), 0.0));
-            for (size_t i = 0; i < matrix.size(); i++)
-            {
-                for (size_t j = 0; j < matrix[i].size(); j++)
-                {
-                    double sum = 0.0;
-                    for (size_t k = 0; k < matrix.size(); k++)
-                    {
-                        sum += matrixPower[i][k] * matrix[k][j];
-                    }
-                    tempPower[i][j] = sum;
-                }
-            }
-            matrixPower = tempPower;
+            matrixPower = matrixPower * matrix;
         }
 
-        Matrix matrixFactor(matrix.size(), std::vector<double>(matrix[0].size(), 0.0));
-        for (size_t i = 0; i < matrix.size(); i++)
-        {
-            for (size_t j = 0; j < matrix[i].size(); j++)
-            {
-                matrixFactor[i][j] = matrixPower[i][j] / fact;
-            }
-        }
-
-        for (size_t i = 0; i < matrix.size(); i++)
-        {
-            for (size_t j = 0; j < matrix[i].size(); j++)
-            {
-                result[i][j] += matrixFactor[i][j];
-            }
-        }
+        Matrix matrixFactor = matrixPower * (1.0 /  fact);
+        result = result + matrixFactor;
 
         if (x.size() == N)
         {
@@ -139,14 +87,8 @@ Matrix calculateExponential(const Matrix& matrix)
             y.erase(y.begin());
         }
         x.push_back(iterNum);
-        double sum = 0.0;
-        for (size_t i = 0; i < matrix.size(); i++)
-        {
-            for (size_t j = 0; j < matrix[i].size(); j++)
-            {
-                sum += matrixFactor[i][j];
-            }
-        }
+        double sum = calculateMatrixSum(matrixFactor);
+
         y.push_back(sum);
 
         if (x.size() == N)
@@ -174,22 +116,24 @@ Matrix calculateExponential(const Matrix& matrix)
 int main()
 {
     std::cout << "Matrix Exponential Calculation Program\n------------------------------------" << std::endl;
-    Matrix inputMatrix = matrixFromFile("inv_matrix(1000x1000).txt");
-    if (!validateSquare(inputMatrix))
+    Matrix inputMatrix("inv_matrix(1000x1000).txt");
+
+    if (inputMatrix.cols != inputMatrix.rows)
     {
         std::cout << "Invalid input. The input matrix must be square." << std::endl;
         return 0;
     }
 
-    std::clock_t startClock = std::clock();
+    std::clock_t startClock = Get_Time();
     Matrix resultMatrix = calculateExponential(inputMatrix);
-    std::clock_t endClock = std::clock();
-    double elapsedTime = static_cast<double>(endClock - startClock) / CLOCKS_PER_SEC;
+    std::clock_t endClock = Get_Time();
+    double elapsedTime = endClock - startClock;
     std::cout << "\nCustom implementation took: " << elapsedTime << " seconds." << std::endl;
+    
     std::cout << "Top-left and bottom-right elements of the resultant matrix are: ["
-        << resultMatrix[0][0] << ", " << resultMatrix[0][1] << ", ..., "
-        << resultMatrix.back()[resultMatrix.back().size() - 2] << ", "
-        << resultMatrix.back().back() << "]" << std::endl;
+        << resultMatrix.p[0] << ", " << resultMatrix.p[1] << ", ..., "
+        << resultMatrix.p[resultMatrix.cols  * resultMatrix.rows - 2] << ", "
+        << resultMatrix.p[resultMatrix.cols * resultMatrix.rows - 1] << "]" << std::endl;
 
     resultToFile(resultMatrix, "result.txt");
 
